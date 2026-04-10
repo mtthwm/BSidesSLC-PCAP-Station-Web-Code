de51f4ab632733d7e736bb53e3beff9926974c5b
Matthew Morales <mtthwmrls9@gmail.com>
2026-04-10 00:52:51 -0600

    Finish webdriver code

--- selenium/fake-client.py
+++ selenium/fake-client.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
+import random
 
 # --- Configuration ---
 URL = "http://localhost:1283/"
@@ -14,31 +15,32 @@ driver = webdriver.Chrome()
 wait = WebDriverWait(driver, 10)
 
 try:
-    driver.get(URL)
+    while True:
+        driver.get(URL)
 
-    sleep(2)
+        sleep(2)
 
-    username_field = wait.until(EC.presence_of_element_located((By.ID, "username")))
-    username_field.clear()
-    username_field.send_keys(USERNAME)
+        username_field = wait.until(EC.presence_of_element_located((By.ID, "username")))
+        username_field.clear()
+        username_field.send_keys(USERNAME)
 
-    sleep(0.75)
+        sleep(0.75)
 
-    password_field = driver.find_element(By.ID, "password")
-    password_field.clear()
-    password_field.send_keys(PASSWORD)
+        password_field = driver.find_element(By.ID, "password")
+        password_field.clear()
+        password_field.send_keys(PASSWORD)
 
-    sleep(1.3)
+        sleep(1.3)
 
-    login_button = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
-    login_button.click()
+        login_button = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
+        login_button.click()
 
-    sleep(3.2)
+        sleep(10)
 
-    post = driver.find_elements(By.CSS_SELECTOR)
-
-    wait.until(EC.url_changes(URL))
-    print(f"Login successful. Redirected to: {driver.current_url}")
+        # Click the logout button
+        logout_btn = driver.find_element(By.CSS_SELECTOR, "a[href='/logout']")
+        logout_btn.click()
+        sleep(random.randint(2, 5))
 
 finally:
     driver.quit()
